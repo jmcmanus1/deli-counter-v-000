5c348eb8c134b9771b2b1ed92b1b8f5066df7e25
Julie  McManus <julieannesays@gmail.com>
2020-01-24 19:28:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,8 +25,10 @@ end
 def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
-  #elsif katz_deli.each do |name|
-  #  katz_deli.shift
-  #  puts "Currently serving #{katz_deli.first}."
+  elsif katz_deli > 0
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.each do |name|
+    katz_deli.shift
+  end
   end
 end
